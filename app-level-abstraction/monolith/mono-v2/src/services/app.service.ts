6cd69f45217e87180320c6622b57036debe2ea7c
BarakMor <barakm@codevalue.com>
2022-05-21 13:06:40 +0300

    add mono v2

---
+++ app-level-abstraction/monolith/mono-v2/src/services/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { BabyDto } from 'src/dtos/baby.dto';
+import { BabyRepository } from 'src/repositories/baby.repository';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly babyRepository: BabyRepository) {}
+  async produceBaby(baby: BabyDto): Promise<BabyDto> {
+    return await this.babyRepository.produceBaby(baby);
+  }
+}

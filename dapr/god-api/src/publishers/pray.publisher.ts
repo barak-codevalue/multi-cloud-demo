62dd7e4c8b5205bcc156993e92e8a3631a452785
BarakMor <barakm@codevalue.com>
2022-06-09 09:11:22 +0300

    rename

--- dapr/god-api/src/publishers/pray.publisher.ts
+++ dapr/god-api/src/publishers/pray.publisher.ts
@@ -8,23 +8,9 @@ const DAPR_Port = process.env.CLIENT_DAPR_Port;
 @Injectable()
 export class PrayBroker {
   daprClient: DaprClient = new DaprClient(DAPR_HOST, DAPR_Port);
-  /**
-   *
-   */
 
   async publish(pray: PrayDto): Promise<PrayDto> {
     this.daprClient.pubsub.publish('prayers-pubsub', 'prayers', pray);
-    // const stateObjects = Object.keys(prayDoc).map((key) => ({
-    //   key: key,
-    //   value: prayDoc[key],
-    // }));
-    // await this.daprClient.state.save('prayers', [
-    //   {
-    //     key: createUuid(),
-    //     value: JSON.stringify(prayDoc),
-    //   },
-    // ]);
-
     return pray;
   }
 }
